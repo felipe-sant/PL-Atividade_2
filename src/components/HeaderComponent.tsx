72c651ae3c61c99167badc71b4052296c9a6beeb
Luiz Felipe dos Santos <felipe.pinghang2@gmail.com>
2023-11-23 09:28:03 -0300

    adicionando header base

---
+++ src/components/HeaderComponent.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable jsx-a11y/alt-text */
+import { Component } from "react";
+import logo from "../images/heartLogo.svg";
+
+class Header extends Component{
+    render() {
+        return (
+            <div className="header">
+                <div className="logo">
+                    <img src={ logo }/>
+                    <h1>Pet Lovers</h1>
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="#">Pedro augusto</a></li>
+                        <li><a href="#">Pedro augusto</a></li>
+                        <li><a href="#">Pedro augusto</a></li>
+                    </ul>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default Header;
